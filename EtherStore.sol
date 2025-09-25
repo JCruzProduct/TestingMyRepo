574a8c8fcfe97d3a002d189d5960415a0437e5d9
JCruzProduct <jesus.cruz+github@immunefi.com>
2025-09-25 16:22:25 +0200

    Update EtherStore.sol
    Demo

--- EtherStore.sol
+++ EtherStore.sol
@@ -7,7 +7,7 @@ contract EtherStore {
     function deposit() public payable {
         balances[msg.sender] += msg.value;
     }
-
+Change for demo 1
     function withdraw() public {
         uint256 bal = balances[msg.sender];
         require(bal >= 0);
@@ -17,7 +17,7 @@ contract EtherStore {
 
         balances[msg.sender] = 0;
     }
-
+Change for demo 2
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
